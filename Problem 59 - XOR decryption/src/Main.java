011abfc01749b0af96ed7e5cce61ddeb429d7a21
dorukBen <dorukerena@gmail.com>
2017-06-09 07:48:35 +0300

    Problem 59 - XOR decryption

---
+++ Problem 59 - XOR decryption/src/Main.java
@@ -0,0 +1,113 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * XOR decryption
+ * Problem 59
+ *
+ * Each character on a computer is assigned a unique code and the preferred standard is ASCII (American Standard Code for
+ * Information Interchange). For example, uppercase A = 65, asterisk (*) = 42, and lowercase k = 107.
+ *
+ * A modern encryption method is to take a text file, convert the bytes to ASCII, then XOR each byte with a given value, taken
+ * from a secret key. The advantage with the XOR function is that using the same encryption key on the cipher text, restores
+ * the plain text; for example, 65 XOR 42 = 107, then 107 XOR 42 = 65.
+ *
+ * For unbreakable encryption, the key is the same length as the plain text message, and the key is made up of random bytes. The
+ * user would keep the encrypted message and the encryption key in different locations, and without both "halves", it is
+ * impossible to decrypt the message.
+ *
+ * Unfortunately, this method is impractical for most users, so the modified method is to use a password as a key. If the
+ * password is shorter than the message, which is likely, the key is repeated cyclically throughout the message. The balance
+ * for this method is using a sufficiently long password key for security, but short enough to be memorable.
+ *
+ * Your task has been made easy, as the encryption key consists of three lower case characters. Using cipher.txt (right click
+ * and 'Save Link/Target As...'), a file containing the encrypted ASCII codes, and the knowledge that the plain text must
+ * contain common English words, decrypt the message and find the sum of the ASCII values in the original text.
+ *
+ * Created by doruk on 8.06.2017.
+ */
+
+public class Main {
+
+    private static final int KEY_LENGTH = 3;
+
+    public static void main(String[] args) throws IOException {
+        char[] message = stringToCharArray(readFile("p059_cipher.txt"));
+        char[] key = findKey(message);
+
+        System.out.print(encryptMessage(message, key));
+    }
+
+    private static int encryptMessage(char[] message, char[] key){
+        int sumOfDigits = 0;
+        for (int i = 0; i < message.length; i++){
+            char digit = (char) (message[i]^ key[i% KEY_LENGTH]);
+            sumOfDigits += digit;
+        }
+
+        return sumOfDigits;
+    }
+
+    private static char[] findKey(char[] chars){
+        char[] key = new char[KEY_LENGTH];
+
+        for (int i = 0; i < KEY_LENGTH; i++){
+            key[i] = 'a';
+        }
+
+        boolean keyFound = false;
+        while (!keyFound){
+            boolean isCharactersValid = true;
+            for (int i = 0; i < chars.length && isCharactersValid; i++){
+                char digit = (char) (chars[i]^ key[i% KEY_LENGTH]);
+                if (!(Character.isAlphabetic(digit) || Character.isDigit(digit) || Character.isSpaceChar(digit) || isPunctuation(digit))){
+                    isCharactersValid = false;
+                    key[i% KEY_LENGTH] = (char) ((key[i% KEY_LENGTH] + 1 <= 'z')? (key[i% KEY_LENGTH] + 1): key[i% KEY_LENGTH]);
+                }
+            }
+
+            if (isCharactersValid){
+                keyFound = true;
+            }
+        }
+
+        return key;
+    }
+
+    private static boolean isPunctuation(char c) {
+        return c == ','
+                || c == '.'
+                || c == '!'
+                || c == '?'
+                || c == ':'
+                || c == ';'
+                || c == '\''
+                || c == '('
+                || c == ')';
+    }
+
+    private static String readFile(String fileName) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                line = br.readLine();
+            }
+            return sb.toString();
+        }
+    }
+
+    private static char[] stringToCharArray(String text){
+        String[] strings = text.split(",");
+        char[] chars = new char[strings.length];
+
+        for (int i = 0; i < strings.length; i++){
+            chars[i] = (char) Integer.parseInt(strings[i]);
+        }
+
+        return chars;
+    }
+}
